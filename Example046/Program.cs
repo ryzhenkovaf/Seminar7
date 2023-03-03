92a214c74a0fd143fd684b37c889751b625f1e14
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-03-03 21:02:52 +0700

    add 046

---
+++ Example046/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размеров m*n, заполненный случайными числами
+//   m = 3, n = 4
+//   1   4   8   19
+//   5  -2  33   -2 
+//  77   3   8    1
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray (int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
